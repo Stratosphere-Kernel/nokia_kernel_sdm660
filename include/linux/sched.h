100572e43e86a99d16c66caa09e2558440d68dbe
taalojarvi <taalojarvi@github.com>
2021-03-01 14:15:33 +0530

    include: Fix some derps

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2212,11 +2212,6 @@ static inline pid_t task_tgid_nr_ns(struct task_struct *tsk, struct pid_namespac
 	return __task_pid_nr_ns(tsk, __PIDTYPE_TGID, ns);
 }
 
-static inline pid_t task_tgid_vnr(struct task_struct *tsk)
-{
-	return __task_pid_nr_ns(tsk, __PIDTYPE_TGID, NULL);
-}
-
 /* static inline pid_t task_ppid_nr_ns(const struct task_struct *tsk, struct pid_namespace *ns)
 {
 	pid_t pid = 0;
